dd4a928669bbbd6911c255fd00044f78d292d625
Facundo Manuel Otero <facu_otero@hotmail.com>
2021-09-01 08:30:14 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div>
+
+    <div>
+      <NavBar />
+    </div>
+    <h1 className="title"> Equipamiento de Rugby </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NavBar/NavBar.css'
+
+function NavBar() {
+    return (
+        <div>
+            <div className="navbar">Esto es el narvar</div>
+        </div>
+    )
+}
+
+export default NavBar
